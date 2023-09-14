3436d5e24a96912a5aeafe6b978de303c703e296
Sina Hatamian <sina.hatamian75@gmail.com>
2023-09-14 20:01:39 +0330

    Ways create and update done

--- src/views/pages/introductionWays/IntroductionWays.js
+++ src/views/pages/introductionWays/IntroductionWays.js
@@ -19,6 +19,7 @@ import {
   CButton,
   CTableDataCell,
   CTableBody,
+  CFormInput,
 } from '@coreui/react'
 
 const IntroductionWays = () => {
@@ -38,6 +39,47 @@ const IntroductionWays = () => {
         console.log(err)
       })
   }, [])
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target
+    setFormdata({
+      ...formdata,
+      [name]: value,
+    })
+  }
+
+  const handleAddWays = () => {
+    AxiosInstance.post('/introduction-ways', formdata)
+      .then((res) => {
+        console.log(res.data.data)
+        setIntroductionWaysList([...introductionWaysList, res.data.data.introduction_way])
+        alert('روش آشنایی با موفقیت افزوده شد')
+        setActivekey(1)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const handleUpdateWays = (id) => {
+    AxiosInstance.put(`/introduction-ways/${id}`, formdata)
+      .then((res) => {
+        console.log(res.data.data)
+        setIntroductionWaysList(
+          introductionWaysList.map((introductionWay) => {
+            if (introductionWay.id === id) {
+              return res.data.data.introduction_way
+            }
+            return introductionWay
+          }),
+        )
+        alert('روش آشنایی با موفقیت ویرایش شد')
+        setActivekey(1)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
   return (
     <>
@@ -73,12 +115,63 @@ const IntroductionWays = () => {
                         <CTableRow key={introductionWay.id}>
                           <CTableDataCell>{introductionWay.title}</CTableDataCell>
                           <CTableDataCell>
-                            <CButton color="primary">ویرایش</CButton>
+                            <CButton
+                              color="primary"
+                              onClick={() => {
+                                setFormdata({
+                                  title: introductionWay.title,
+                                  id: introductionWay.id,
+                                })
+                                setActivekey(2)
+                              }}
+                            >
+                              ویرایش
+                            </CButton>
                           </CTableDataCell>
                         </CTableRow>
                       ))}
                     </CTableBody>
                   </CTable>
+                </CCardBody>
+              </CCard>
+            </CCol>
+          </CRow>
+        </CTabPane>
+        <CTabPane role="tabpanel" aria-labelledby="profile-tab" visible={activekey === 2}>
+          <CRow>
+            <CCol xs="12">
+              <CCard className="mb-4">
+                <CCardHeader>
+                  <strong>ثبت روش آشنایی جدید</strong>
+                </CCardHeader>
+                <CCardBody>
+                  <CRow>
+                    <CCol xs="12" md={6}>
+                      <CFormInput
+                        label="نام روش آشنایی"
+                        placeholder="نام روش آشنایی"
+                        aria-label="title"
+                        locale="fa-IR"
+                        value={formdata.title}
+                        name="title"
+                        onChange={handleChangeInput}
+                      />
+                    </CCol>
+                    <CCol xs="12">
+                      <CButton
+                        color="success"
+                        onClick={() => {
+                          if (formdata.id) {
+                            handleUpdateWays(formdata.id)
+                          } else {
+                            handleAddWays()
+                          }
+                        }}
+                      >
+                        ثبت
+                      </CButton>
+                    </CCol>
+                  </CRow>
                 </CCardBody>
               </CCard>
             </CCol>
