112669178b42dc8ca9b7a59f3ff978e8a15abc0d
Sina Hatamian <sina.hatamian75@gmail.com>
2024-03-14 13:20:09 +0330

    Fix type 6 payment

--- src/views/pages/chargeForm/ChargeForm.js
+++ src/views/pages/chargeForm/ChargeForm.js
@@ -180,7 +180,13 @@ const ChargeForm = () => {
                   {chargeForm.map((item) => (
                     <CTableRow key={item.id}>
                       <CTableDataCell>
-                        {item.type == '2' ? 'پوز' : item.type == '3' ? ' نقدی ' : 'رایگان'}
+                        {item.type == '2'
+                          ? 'پوز'
+                          : item.type == '3'
+                          ? ' نقدی '
+                          : item.type == '6'
+                          ? 'هدیه'
+                          : ' رایگان '}
                       </CTableDataCell>
                       <CTableDataCell>
                         {item.status == '1' ? 'تایید شده' : 'تایید نشده'}
