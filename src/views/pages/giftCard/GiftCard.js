5ded9a9d0a7854b5f9fddfae76e3f4d8941e4260
Sina Hatamian <sina.hatamian75@gmail.com>
2024-03-14 12:53:44 +0330

    Add pagination to gift card table

--- src/views/pages/giftCard/GiftCard.js
+++ src/views/pages/giftCard/GiftCard.js
@@ -23,11 +23,17 @@ import {
   CTableDataCell,
   CTableRow,
   CFormCheck,
+  CPagination,
+  CPaginationItem,
 } from '@coreui/react'
 
 const GiftCard = () => {
   const [activeKey, setActiveKey] = useState(1)
   const [card, setCard] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const itemsPerPage = 30
+  const startIndex = (currentPage - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
   const [formdata, setFormdata] = useState({
     numbers: [''],
     amount: '',
@@ -37,6 +43,7 @@ const GiftCard = () => {
   })
 
   useEffect(() => {
+    console.log('Current Page:', currentPage)
     AxiosInstance.get('/gift-cards')
       .then((res) => {
         console.log(res)
@@ -45,7 +52,7 @@ const GiftCard = () => {
       .catch((err) => {
         console.log(err)
       })
-  }, [])
+  }, [currentPage])
 
   const handleInputCahnge = (e) => {
     const { name, value } = e.target
@@ -240,14 +247,14 @@ const GiftCard = () => {
                 <CTableHead>
                   <CTableRow>
                     <CTableHeaderCell>کد کارت</CTableHeaderCell>
-                    <CTableHeaderCell>مبلغ</CTableHeaderCell>
+                    <CTableHeaderCell>مبلغ (ریال)</CTableHeaderCell>
                     <CTableHeaderCell>توضیحات</CTableHeaderCell>
                     <CTableHeaderCell>وضعیت</CTableHeaderCell>
                     <CTableHeaderCell>دستور دهنده</CTableHeaderCell>
                   </CTableRow>
                 </CTableHead>
                 <CTableBody>
-                  {card.map((item) => (
+                  {card.slice(startIndex, endIndex).map((item) => (
                     <CTableRow key={item.id}>
                       <CTableDataCell>{item.number}</CTableDataCell>
                       <CTableDataCell>
@@ -308,6 +315,64 @@ const GiftCard = () => {
                   ))}
                 </CTableBody>
               </CTable>
+              <CPagination
+                align="center"
+                doubleArrows={false}
+                activePage={currentPage}
+                pages={Math.ceil(card.length / itemsPerPage)}
+                onActivePageChange={setCurrentPage}
+                limit={5}
+              >
+                <CPaginationItem
+                  onClick={() => {
+                    if (currentPage > 1) {
+                      setCurrentPage(currentPage - 1)
+                    }
+                  }}
+                >
+                  قبلی
+                </CPaginationItem>
+
+                {/* Generate a CPaginationItem for each page */}
+                {[...Array(Math.ceil(card.length / itemsPerPage)).keys()].map((i) => {
+                  // Only create CPaginationItem for the first page, the last page, the current page, and two pages around the current page
+                  if (
+                    i === 0 ||
+                    i === Math.ceil(card.length / itemsPerPage) - 1 ||
+                    Math.abs(currentPage - (i + 1)) <= 2
+                  ) {
+                    return (
+                      <CPaginationItem
+                        key={i}
+                        active={i + 1 === currentPage}
+                        onClick={() => setCurrentPage(i + 1)}
+                      >
+                        {i + 1}
+                      </CPaginationItem>
+                    )
+                  } else if (i === 1 && currentPage > 4) {
+                    // Add an ellipsis after the first page if the current page is greater than 4
+                    return <CPaginationItem key={i}>...</CPaginationItem>
+                  } else if (
+                    i === Math.ceil(card.length / itemsPerPage) - 2 &&
+                    currentPage < Math.ceil(card.length / itemsPerPage) - 3
+                  ) {
+                    // Add an ellipsis before the last page if the current page is less than the total number of pages minus 3
+                    return <CPaginationItem key={i}>...</CPaginationItem>
+                  } else {
+                    return null
+                  }
+                })}
+                <CPaginationItem
+                  onClick={() => {
+                    if (currentPage < Math.ceil(card.length / itemsPerPage)) {
+                      setCurrentPage(currentPage + 1)
+                    }
+                  }}
+                >
+                  بعدی
+                </CPaginationItem>
+              </CPagination>
             </CCardBody>
           </CCard>
         </CTabPane>
