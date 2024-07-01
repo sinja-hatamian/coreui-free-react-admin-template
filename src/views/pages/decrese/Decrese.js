cd53799991d1d3faf204bad5588ba20c15853ec3
Sina Hatamian <sina.hatamian75@gmail.com>
2024-07-01 14:47:54 +0330

    Add balance to Decrese

--- src/views/pages/decrese/Decrese.js
+++ src/views/pages/decrese/Decrese.js
@@ -1,23 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AxiosInstance from 'src/utils/AxiosInstance'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import {
-  CNav,
-  CNavItem,
-  CNavLink,
-  CTabContent,
-  CTabPane,
-  CRow,
-  CCol,
-  CCardHeader,
-  CCardBody,
-  CCard,
-  CForm,
-  CFormInput,
-  CFormSelect,
-  CButton,
-} from '@coreui/react'
+import { CRow, CCol, CCardHeader, CCardBody, CCard, CFormInput, CButton } from '@coreui/react'
+import { Axios } from 'axios'
 
 const Decrese = () => {
   const [formData, setFormData] = useState({
@@ -25,22 +11,47 @@ const Decrese = () => {
     amount: '',
     description: '',
   })
-  const [nationalCode, setNationalCode] = useState('')
   const [customerData, setCustomerData] = useState({
     firstname: '',
     lastname: '',
   })
+  const [nationalCode, setNationalCode] = useState('')
+  const [card, setCard] = useState({})
 
   const handleInput = (e) => {
+    const rawNumber = e.target.value
+    const name = e.target.name
+    const value = rawNumber.replace(/[^0-9]/g, '')
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value,
+      [name]: value,
     })
   }
+
+  // useEffect(() => {
+  //   if (localStorage.getItem('customer')) {
+  //     const customer = JSON.parse(localStorage.getItem('customer'))
+  //     AxiosInstance.get(`/cards/${customer.id}`)
+  //       .then((res) => {
+  //         setCard(res.data.data.card)
+  //       })
+  //       .catch((err) => {
+  //         console.log(err)
+  //         toast.error('خطا در دریافت اطلاعات کارت')
+  //       })
+  //   }
+  // }, [])
 
   const fetchUser = () => {
     AxiosInstance.get(`/users/national-code/${nationalCode}`)
       .then((res) => {
+        AxiosInstance.get(`/cards/${res.data.data.user.id}`)
+          .then((res) => {
+            setCard(res.data.data.card)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
         const customerData = res.data.data.user
         setCustomerData(customerData)
         setFormData({
@@ -64,6 +75,10 @@ const Decrese = () => {
         console.log(err)
         toast.error('خطا رخ داد')
       })
+  }
+
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   }
 
   return (
@@ -92,21 +107,36 @@ const Decrese = () => {
                 <div
                   className="text-center"
                   style={{
-                    backgroundColor: '#f0f0f0',
+                    border: '1px solid #ccc',
                     padding: '10px',
                     borderRadius: '5px',
-                    border: '1px solid #ccc',
+                    backgroundColor: '#f9f9f9',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center', // Corrected from 'justifyItems' to 'alignItems' for proper CSS
+                    flexDirection: 'column', // Added for vertical alignment of items
                   }}
                 >
                   مشخصات مشتری:
                   <br />
                   {customerData.firstname + ' ' + customerData.lastname}
+                  <p />
+                  <p>
+                    موجودی کارت: {card.balance ? numberWithCommas(card.balance) : 0} ریال
+                    <br />
+                    شماره کارت: {card.number}
+                  </p>
                 </div>
               </CCol>
 
               {/* Transaction Section */}
               <CCol md="6" className="mt-3">
-                <CFormInput name="amount" placeholder="مبلغ" onChange={handleInput} />
+                <CFormInput
+                  name="amount"
+                  placeholder="مبلغ"
+                  onChange={handleInput}
+                  value={numberWithCommas(formData.amount)}
+                />
               </CCol>
               <CCol md="6" className="mt-3">
                 <CFormInput name="description" placeholder="توضیحات" onChange={handleInput} />
