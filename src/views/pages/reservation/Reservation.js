1b31003870afca5842d5b289a8faf4b4c1a6cf8d
Sina Hatamian <sina.hatamian75@gmail.com>
2024-07-08 14:04:13 +0330

    Reservation show done

--- src/views/pages/reservation/Reservation.js
+++ src/views/pages/reservation/Reservation.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import AxiosInstance from 'src/utils/AxiosInstance'
 import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+
 import {
   CCol,
   CRow,
@@ -28,12 +28,12 @@ import {
   CTabContent,
   CTabPane,
 } from '@coreui/react'
-import { Axios } from 'axios'
 
 const Reservation = () => {
   const [stages, setStages] = useState([])
   const [activeKey, setActiveKey] = useState(1)
   const [nationalCode, setNationalCode] = useState('')
+  const [phone, setPhone] = useState('')
   const [formData, setFormData] = useState([
     {
       firstname: '',
@@ -59,6 +59,23 @@ const Reservation = () => {
 
   const fetchUser = () => {
     AxiosInstance.get(`/users/national-code/${nationalCode}`)
+      .then((res) => {
+        AxiosInstance.get(`/reserves/users/${res.data.data.user.id}`)
+          .then((res) => {
+            setFormData(res.data.data.reserves)
+            console.log(res.data.data.reserves)
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  const fetchUSerByPhone = () => {
+    AxiosInstance.get(`/users/phone/${phone}`)
       .then((res) => {
         AxiosInstance.get(`/reserves/users/${res.data.data.user.id}`)
           .then((res) => {
@@ -106,6 +123,11 @@ const Reservation = () => {
         console.log(error)
       })
   }
+
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  }
+
   return (
     <>
       <CNav variant="tabs" role="tablist">
@@ -133,55 +155,82 @@ const Reservation = () => {
       <CTabContent>
         <CTabPane role="tabpanel" visible={activeKey === 1} aria-labelledby="home-tab">
           <CRow>
-            <CCard className="mb-4">
-              <CCol sm="12">
-                <CCol md="8">
-                  <CButton
-                    color="primary"
-                    onClick={() => {
-                      indexAllReservations()
-                    }}
-                  >
-                    نمایش همه رزروها
-                  </CButton>
-                </CCol>
-                <CTable striped>
-                  <CTableHead>
-                    <CTableRow>
-                      <CTableHeaderCell>نام</CTableHeaderCell>
-                      <CTableHeaderCell>نام خانوادگی</CTableHeaderCell>
-                      <CTableHeaderCell>تاریخ</CTableHeaderCell>
-                      <CTableHeaderCell>عنوان</CTableHeaderCell>
-                      <CTableHeaderCell>زمان شروع</CTableHeaderCell>
-                      <CTableHeaderCell>زمان پایان</CTableHeaderCell>
-                      <CTableHeaderCell>تعداد حاضرین</CTableHeaderCell>
-                      <CTableHeaderCell>قیمت کل</CTableHeaderCell>
-                    </CTableRow>
-                  </CTableHead>
-                  <CTableBody>
-                    {formData.map((item, index) => (
-                      <CTableRow key={index}>
-                        <CTableDataCell>{item.firstname}</CTableDataCell>
-                        <CTableDataCell>{item.lastname}</CTableDataCell>
-                        <CTableDataCell>{item.date}</CTableDataCell>
-                        <CTableDataCell>{item.title}</CTableDataCell>
-                        <CTableDataCell>{item.start_time}</CTableDataCell>
-                        <CTableDataCell>{item.end_time}</CTableDataCell>
-                        <CTableDataCell>{item.attendants_number}</CTableDataCell>
-                        <CTableDataCell>{item.total_price}</CTableDataCell>
+            <CCol xs="12" sm="12" md="12">
+              <CCard className="mb-4">
+                <CCol sm="12">
+                  <CCol md="8" style={{ marginBottom: '20px', padding: '20px' }}>
+                    <CButton
+                      color="primary"
+                      onClick={() => {
+                        indexAllReservations()
+                      }}
+                    >
+                      نمایش همه رزروها
+                    </CButton>
+                  </CCol>
+                  <CTable striped>
+                    <CTableHead>
+                      <CTableRow>
+                        <CTableHeaderCell>نام</CTableHeaderCell>
+                        <CTableHeaderCell>نام خانوادگی</CTableHeaderCell>
+                        <CTableHeaderCell>تاریخ</CTableHeaderCell>
+                        <CTableHeaderCell>عنوان</CTableHeaderCell>
+                        <CTableHeaderCell>زمان شروع</CTableHeaderCell>
+                        <CTableHeaderCell>زمان پایان</CTableHeaderCell>
+                        <CTableHeaderCell>تعداد حاضرین</CTableHeaderCell>
+                        <CTableHeaderCell>قیمت کل</CTableHeaderCell>
                       </CTableRow>
-                    ))}
-                  </CTableBody>
-                </CTable>
-              </CCol>
-            </CCard>
+                    </CTableHead>
+                    <CTableBody>
+                      {formData.map((item, index) => (
+                        <CTableRow key={index}>
+                          <CTableDataCell>{item.firstname}</CTableDataCell>
+                          <CTableDataCell>{item.lastname}</CTableDataCell>
+                          <CTableDataCell>
+                            {
+                              //show date in persian
+                              item.date ? new Date(item.date).toLocaleDateString('fa-IR') : ''
+                            }
+                          </CTableDataCell>
+                          <CTableDataCell>{item.title}</CTableDataCell>
+                          <CTableDataCell>
+                            {
+                              //show start time in persian and not show seconds
+                              item.start_time
+                                ? new Date(item.start_time).toLocaleTimeString('fa-IR', {
+                                    hour: '2-digit',
+                                    minute: '2-digit',
+                                  })
+                                : ''
+                            }
+                          </CTableDataCell>
+                          <CTableDataCell>
+                            {
+                              //show end time in persian and not show seconds
+                              item.end_time
+                                ? new Date(item.end_time).toLocaleTimeString('fa-IR', {
+                                    hour: '2-digit',
+                                    minute: '2-digit',
+                                  })
+                                : ''
+                            }
+                          </CTableDataCell>
+                          <CTableDataCell>{item.attendants_number}</CTableDataCell>
+                          <CTableDataCell>{numberWithCommas(item.total_price)}</CTableDataCell>
+                        </CTableRow>
+                      ))}
+                    </CTableBody>
+                  </CTable>
+                </CCol>
+              </CCard>
+            </CCol>
           </CRow>
         </CTabPane>
         <CTabPane role="tabpanel" visible={activeKey === 2} aria-labelledby="profile-tab">
           <CRow>
             <CCard className="mb-4">
               <CCol sm="12">
-                <CCol md="8">
+                <CCol md="8" style={{ marginBottom: '20px', padding: '20px' }}>
                   <CButton color="primary" onClick={indexTodayReservations}>
                     نمایش رزروهای امروز
                   </CButton>
@@ -204,12 +253,37 @@ const Reservation = () => {
                       <CTableRow key={index}>
                         <CTableDataCell>{item.firstname}</CTableDataCell>
                         <CTableDataCell>{item.lastname}</CTableDataCell>
-                        <CTableDataCell>{item.date}</CTableDataCell>
+                        <CTableDataCell>
+                          {
+                            //show date in persian
+                            item.date ? new Date(item.date).toLocaleDateString('fa-IR') : ''
+                          }
+                        </CTableDataCell>
                         <CTableDataCell>{item.title}</CTableDataCell>
-                        <CTableDataCell>{item.start_time}</CTableDataCell>
-                        <CTableDataCell>{item.end_time}</CTableDataCell>
+                        <CTableDataCell>
+                          {
+                            //show start time in persian and not show seconds
+                            item.start_time
+                              ? new Date(item.start_time).toLocaleTimeString('fa-IR', {
+                                  hour: '2-digit',
+                                  minute: '2-digit',
+                                })
+                              : ''
+                          }
+                        </CTableDataCell>
+                        <CTableDataCell>
+                          {
+                            //show end time in persian and not show seconds
+                            item.end_time
+                              ? new Date(item.end_time).toLocaleTimeString('fa-IR', {
+                                  hour: '2-digit',
+                                  minute: '2-digit',
+                                })
+                              : ''
+                          }
+                        </CTableDataCell>
                         <CTableDataCell>{item.attendants_number}</CTableDataCell>
-                        <CTableDataCell>{item.total_price}</CTableDataCell>
+                        <CTableDataCell>{numberWithCommas(item.total_price)}</CTableDataCell>
                       </CTableRow>
                     ))}
                   </CTableBody>
@@ -221,6 +295,15 @@ const Reservation = () => {
         <CTabPane role="tabpanel" visible={activeKey === 3} aria-labelledby="profile-tab">
           <CRow>
             <CCard className="mb-4">
+              <div
+                style={{
+                  marginBottom: '20px',
+                  padding: '20px',
+                  borderBottom: '1px solid #ccc',
+                }}
+              >
+                <strong>جستجو رزرو بر اساس کد ملی یا شماره تلفن کاربر</strong>
+              </div>
               <CCol sm="12">
                 <CRow>
                   <CCol md="4">
@@ -236,8 +319,27 @@ const Reservation = () => {
                     </CButton>
                   </CCol>
                 </CRow>
-
-                <CTable striped>
+                <br />
+                <CRow>
+                  <CCol md="4">
+                    <CFormInput
+                      placeholder="شماره تلفن"
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                    />
+                  </CCol>
+                  <CCol md="4">
+                    <CButton color="primary" onClick={fetchUSerByPhone}>
+                      جستجو
+                    </CButton>
+                  </CCol>
+                </CRow>
+                <CTable
+                  striped
+                  style={{
+                    marginTop: '20px',
+                  }}
+                >
                   <CTableHead>
                     <CTableRow>
                       <CTableHeaderCell>نام</CTableHeaderCell>
@@ -255,12 +357,37 @@ const Reservation = () => {
                       <CTableRow key={index}>
                         <CTableDataCell>{item.firstname}</CTableDataCell>
                         <CTableDataCell>{item.lastname}</CTableDataCell>
-                        <CTableDataCell>{item.date}</CTableDataCell>
+                        <CTableDataCell>
+                          {
+                            //show date in persian
+                            item.date ? new Date(item.date).toLocaleDateString('fa-IR') : ''
+                          }
+                        </CTableDataCell>
                         <CTableDataCell>{item.title}</CTableDataCell>
-                        <CTableDataCell>{item.start_time}</CTableDataCell>
-                        <CTableDataCell>{item.end_time}</CTableDataCell>
+                        <CTableDataCell>
+                          {
+                            //show start time in persian and not show seconds
+                            item.start_time
+                              ? new Date(item.start_time).toLocaleTimeString('fa-IR', {
+                                  hour: '2-digit',
+                                  minute: '2-digit',
+                                })
+                              : ''
+                          }
+                        </CTableDataCell>
+                        <CTableDataCell>
+                          {
+                            //show end time in persian and not show seconds
+                            item.end_time
+                              ? new Date(item.end_time).toLocaleTimeString('fa-IR', {
+                                  hour: '2-digit',
+                                  minute: '2-digit',
+                                })
+                              : ''
+                          }
+                        </CTableDataCell>
                         <CTableDataCell>{item.attendants_number}</CTableDataCell>
-                        <CTableDataCell>{item.total_price}</CTableDataCell>
+                        <CTableDataCell>{numberWithCommas(item.total_price)}</CTableDataCell>
                       </CTableRow>
                     ))}
                   </CTableBody>
@@ -272,8 +399,23 @@ const Reservation = () => {
         <CTabPane role="tabpanel" visible={activeKey === 4} aria-labelledby="profile-tab">
           <CRow>
             <CCard className="mb-4">
+              <div
+                style={{
+                  marginBottom: '20px',
+                  padding: '20px',
+                  borderBottom: '1px solid #ccc',
+                }}
+              >
+                <strong>جستجو رزرو براساس نام سالن ها</strong>
+              </div>
               <CCol sm="12">
-                <CCol md="8">
+                <CCol
+                  md="8"
+                  style={{
+                    marginBottom: '20px',
+                    padding: '20px',
+                  }}
+                >
                   <CFormSelect
                     name="stage_id"
                     onChange={(e) => indexReservationByStage(e.target.value)}
@@ -305,12 +447,37 @@ const Reservation = () => {
                       <CTableRow key={index}>
                         <CTableDataCell>{item.firstname}</CTableDataCell>
                         <CTableDataCell>{item.lastname}</CTableDataCell>
-                        <CTableDataCell>{item.date}</CTableDataCell>
+                        <CTableDataCell>
+                          {
+                            //show date in persian
+                            item.date ? new Date(item.date).toLocaleDateString('fa-IR') : ''
+                          }
+                        </CTableDataCell>
                         <CTableDataCell>{item.title}</CTableDataCell>
-                        <CTableDataCell>{item.start_time}</CTableDataCell>
-                        <CTableDataCell>{item.end_time}</CTableDataCell>
+                        <CTableDataCell>
+                          {
+                            //show start time in persian and not show seconds
+                            item.start_time
+                              ? new Date(item.start_time).toLocaleTimeString('fa-IR', {
+                                  hour: '2-digit',
+                                  minute: '2-digit',
+                                })
+                              : ''
+                          }
+                        </CTableDataCell>
+                        <CTableDataCell>
+                          {
+                            //show end time in persian and not show seconds
+                            item.end_time
+                              ? new Date(item.end_time).toLocaleTimeString('fa-IR', {
+                                  hour: '2-digit',
+                                  minute: '2-digit',
+                                })
+                              : ''
+                          }
+                        </CTableDataCell>
                         <CTableDataCell>{item.attendants_number}</CTableDataCell>
-                        <CTableDataCell>{item.total_price}</CTableDataCell>
+                        <CTableDataCell>{numberWithCommas(item.total_price)}</CTableDataCell>
                       </CTableRow>
                     ))}
                   </CTableBody>
