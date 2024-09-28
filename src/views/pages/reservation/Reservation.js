171900ac93f1da76fad5f181d6267497391beb76
Sina Hatamian <sina.hatamian75@gmail.com>
2024-09-28 15:56:55 +0330

    Cleaning

--- src/views/pages/reservation/Reservation.js
+++ src/views/pages/reservation/Reservation.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import AxiosInstance from 'src/utils/AxiosInstance'
 import { ToastContainer, toast } from 'react-toastify'
-
 import {
   CCol,
   CRow,
